816acec51b9368d34965e4a5d04a21753d8b98bc
Alumno1 <alumno1@anahuacmayab.edu.mx>
2025-10-16 19:42:37 -0600

    Finalizado con componentes

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import {
-  Modal,
   StyleSheet,
   Text,
-  TextInput,
   TouchableOpacity,
   View
 } from 'react-native'
 
+import AddNoteModal from '@/components/AddNoteModal'
 import NoteList from '@/components/NotesList'
 
 const NoteScreen = () => {
@@ -43,45 +42,13 @@ const NoteScreen = () => {
       </TouchableOpacity>
 
       {/* Creamos la ventana Modal */} 
-      <Modal
-        visible={modalVisible}
-        animationType='slide'
-        transparent
-        onRequestClose={()=> setModalVisible(false)}
-      >
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>
-              Agregar una Nota Nueva
-            </Text>
-            <TextInput
-              style={styles.input}
-              placeholder='Teclea una descripción'
-              placeholderTextColor={'#aaa'}
-              onChangeText={setNewNote}
-            />
-            <View style={styles.modalButtons}>
-              {/* Boton Cancelar */}
-              <TouchableOpacity 
-                style={styles.cancelButton}
-                onPress={()=>setModalVisible(false)}
-              >
-                <Text style={styles.cancelButtonText}>Cancelar</Text>
-              </TouchableOpacity>
-
-              {/* Boton Aceptar */}
-              <TouchableOpacity 
-                style={styles.saveButton}
-                onPress={addNewNote}
-              >
-                <Text style={styles.saveButtonText}>Agregar</Text>
-              </TouchableOpacity>
-
-
-            </View>
-          </View>
-        </View>
-      </Modal> 
+      <AddNoteModal 
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        newNote={newNote}
+        setNewNote={setNewNote}
+        addNewNote={addNewNote}
+      />
 
     </View>
   )
@@ -121,58 +88,7 @@ const styles = StyleSheet.create({
       fontSize:18,
       fontWeight: 'bold'
     },
-    modalButtons: {
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    cancelButton: {
-      backgroundColor: '#ccc',
-      padding: 10,
-      borderRadius:5,
-      flex:1,
-      marginRight:10,
-      alignItems: 'center'
-    },
-    cancelButtonText: {
-      fontSize:16,
-      color:'#00000000'
-    },
-     saveButton: {
-      backgroundColor: '#007bff',
-      padding: 10,
-      borderRadius:5,
-      flex:1,
-      marginRight:10,
-      alignItems: 'center'
-    },
-    saveButtonText: {
-      fontSize:16,
-      color:'#fff'
-    },
-    modalOverlay: {
-      flex: 1,
-      backgroundColor: '#333',
-      justifyContent: 'center',
-      alignItems:'center'
-    },
-    modalContent: {
-      backgroundColor: '#fff',
-      padding:20,
-      borderRadius: 10,
-      width: '80%'
-    },
-    modalTitle:{
-      fontWeight:'bold',
-      marginBottom:10,
-      textAlign:'center',
-      fontSize:20
-    },
-    input: {
-      borderWidth:1,
-      borderRadius:8,
-      padding:10,
-      fontSize:16,
-      marginBottom:15
-    }
+    
+    
 
 })
